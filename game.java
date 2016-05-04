c60e37f61f3246fad83923b6efbf93ff60d650cf
rakeshvb <rakeshvb@usc.edu>
2016-05-03 18:13:17 -0700

    blank frame. initial commit

---
+++ game.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cows_bulls;
+import java.io.*;
+import java.util.*;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import javax.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Rakesh Bora
+ */
+ class game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException
+    {
+        JFrame myFrame = new JFrame("Sample Frame");
+        myFrame.setSize(300,400);
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+    }
+}
